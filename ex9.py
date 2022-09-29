29e03a1c274f11ce4bb75b32eb82d3db3ed62d5b
Keith Taylor <k-taylor@outlook.com>
2022-09-29 16:47:14 +0100

    ex9 finished

---
+++ ex9.py
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+print(""""
+There's something going on here.
+With the three string double-quotes.
+We'll be able to type as much as we like. 
+Even four lines if we want, or five or six.
+""")
