7ec9552585878a40a1a2c0c358cd9d1db5ba4aee
Keith Taylor <k-taylor@outlook.com>
2022-09-29 16:18:42 +0100

    ex7 complete

---
+++ ex7.py
@@ -0,0 +1,23 @@
+print("Mary had a little lamb, ")
+print("Its fleece was white as {}, ".format('snow'))
+print("And eveywhere that Mary went, ")
+print("That lamb was sure to go. ")
+print("."*10)
+
+end_1 = "C"
+end_2 = "h"
+end_3 = "e"
+end_4 = "e"
+end_5 = "s"
+end_6 = "e"
+end_7 = "B"
+end_8 = "u"
+end_9 = "r"
+end_10 = "g"
+end_11 = "e"
+end_12 = "r"
+
+print(end_1 + end_2 + end_3 + end_4 + end_5 + end_6, end=' ')
+
+print(end_7 + end_8 + end_9 + end_10 + end_11 + end_12)
+
