16fef166df9b39df75605c91e13150ff085a2783
Keith Taylor <k-taylor@outlook.com>
2022-09-29 16:34:04 +0100

    ex8 complete

---
+++ ex8.py
@@ -0,0 +1,13 @@
+formatter = "{} {} {} {}"
+
+print(formatter.format(1,2,3,4))
+print(formatter.format('one','two','three','four'))
+print(formatter.format(True, False, False, True))
+print(formatter.format(
+    "Try your own",
+    "text",
+    "in here Sir!",
+    "but it needs be four Sir!"
+))
+
+
