2797a5f405db3f2e4a2c20c00f944b6a6fc7b7c2
Keith Taylor <k-taylor@outlook.com>
2022-10-14 12:52:28 +0100

    adding ex39

--- ex39.py
+++ ex39.py
@@ -1,3 +1,43 @@
-from os import lseek
+states = {
+    'Oergon': 'OR',
+    'Florida': 'FL',
+    'California': 'CA',
+    'New York': 'NY',
+    'Michegan': 'MI',
+    'Illinois': 'IL',
+    'Washington': 'WA',
+    'Pennsylvania': 'PA'
+}
+
+cities = {
+    'CA': ['San Franciso', 'Los Angeles', 'Los Alamos', 'Sacramento'],
+    'MI': ["Detroit"], 
+    'FL': ['Jacksonville'],
+    'NY': ['New York', 'Albany'],
+    'OR': ['Portland'],
+    'IL': ['Chicago'],
+    'WA': ['Seatle'],
+    'PA': ['Philadelphia']
+}
 
 
+#print(cities)
+
+# print('-' * 10)
+# print("NY State has: ", cities['NY'])
+# print("OR State has: ", cities['OR'])
+
+# print('-' * 10)
+# print("Michegan's abbreviation is: ", states['Michegan'])
+# print("Florida's abbreviation is: ", states['Florida'])
+
+# print('-' * 10)
+# print("Michegan has: ", cities[states['Michegan']])
+# print("Florida has: ", cities[states['Florida']])
+
+
+for each_state in states:
+    cities_list = []
+    for each_city in cities[states[each_state]]:
+        cities_list.append(each_city)
+    print(f"These are the listed cities for {each_state} state: {', '.join(cities_list)}. ")
