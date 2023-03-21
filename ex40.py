fcb0a2f912093f984c7358d38f5d7cf35b467562
Keith Taylor <k-taylor@outlook.com>
2023-03-21 13:25:20 +0000

    Uploading for sync

---
+++ ex40.py
@@ -0,0 +1,14 @@
+import ex40_stuff
+
+my_stuff = {
+'apples': "My apples. ",   
+'bananas': "These are my bananas. "
+}
+
+print(my_stuff['apples'])
+print(my_stuff['bananas'])
+
+ex40_stuff.apples()
+ex40_stuff.bananas(4)
+
+print(f"Tangerine: {ex40_stuff.tangerine}. ")
---
+++ ex40_stuff.py
@@ -0,0 +1,7 @@
+def apples():
+    print("My apples. ")
+
+def bananas(quantity):
+    print(f"These are my {quantity} bananas. ")
+    
+tangerine = "Living reflection, of a dream"
